12b2b8bdb24e5f40c6417dc60134ee98a6c9b368
ryucd <rbjp2014@gmail.com>
2015-11-24 22:35:02 +0900

    enable token kinds on parser

--- gettoken.c
+++ gettoken.c
@@ -5,15 +5,14 @@
 int gettoken(char* token, int len)
 {
 	int i;
+	*token = '\0';
 	for(i = 0; i < len; i++){
 		char c = getc(stdin);
 	
 		switch(c){
 			case '|':
 				// pipe
-				if(i != 0){
-					*token++ = '|';
-					*token = '\0';
+				if(i == 0){
 					return (int)TKN_PIPE;
 				} else {
 					ungetc(c, stdin);
@@ -22,7 +21,8 @@ int gettoken(char* token, int len)
 				}
 				break;
 			case '>':
-				if(i != 0){
+				if(i == 0){
+					return (int)TKN_REDIR_OUT;
 				} else {
 					ungetc(c, stdin);
 					*token = '\0';
@@ -30,7 +30,8 @@ int gettoken(char* token, int len)
 				}
 				break;
 			case '<':
-				if(i != 0){
+				if(i == 0){
+					return (int)TKN_REDIR_IN;
 				} else {
 					ungetc(c, stdin);
 					*token = '\0';
@@ -38,7 +39,8 @@ int gettoken(char* token, int len)
 				}
 				break;
 			case '&':
-				if(i != 0){
+				if(i == 0){
+					return (int)TKN_BG;
 				} else {
 					ungetc(c, stdin);
 					*token = '\0';
@@ -46,12 +48,22 @@ int gettoken(char* token, int len)
 				}
 				break;
 			case '\n':
-				*token = '\0';
-				return (int)TKN_EOL;
+				if(i == 0){
+					return (int)TKN_EOL;
+				} else {
+					ungetc(c, stdin);
+					*token = '\0';
+					return (int)TKN_NORMAL;
+				}
 				break;
 			case '\0':
-				*token = '\0';
-				return (int)TKN_EOF;
+				if(i == 0){
+					return (int)TKN_EOF;
+				} else {
+					ungetc(c, stdin);
+					*token = '\0';
+					return (int)TKN_EOF;
+				}
 				break;
 			default:
 				*token++ = c;
--- main.c
+++ main.c
@@ -7,53 +7,102 @@
 #include <sys/wait.h>
 #include "consts.h"
 
+int check_state(enum TKN_KIND, char*);
+void run_command(int, char*[]);
+
+static int background = 0;
+static int argc;
+static char *argv[MAX_ARGS];
+static char lbuf[MAX_LEN];
 int main()
 {
-	int argc;
 	int status;
-	int background;
-	char input[MAX_LEN], *argv[MAX_ARGS], *cp;
+	char input[MAX_LEN], *cp;
 	enum TKN_KIND st;
 
 	while (1) {
 		background = 0;
 		printf("$ "); 
 		
-		/*if(fgets(input, sizeof(input), stdin) == NULL){
-			exit(0);
-		}*/
+		while(1){
+			st = gettoken(input, sizeof(input));
+
+			if(check_state(st, input) == 1){
+				if(argc == 0) continue;
+
+				if(strcmp(argv[argc-1],"&") == 0){
+					background = 1;
+					argv[argc-1] = NULL;
+				}
+
+				if(strcmp(argv[0], "cd") == 0){
+					chdir(argv[1]);
+					continue;
+				} else if(strcmp(argv[0], "exit") == 0){
+					exit(0);
+				}
+
+				pid_t pid = fork();
 		
-		st = gettoken(input, sizeof(input));
-		printf("input:%s\n", input);
-		
-		if(*input == '\0') continue;
-		getargs(&argc, argv, input);
-
-		if(argc == 0) continue;
-
-		if(strcmp(argv[argc-1],"&") == 0){
-			background = 1;
-			argv[argc-1] = NULL;
-		}
-
-		if(strcmp(argv[0], "cd") == 0){
-			chdir(argv[1]);
-			continue;
-		} else if(strcmp(argv[0], "exit") == 0){
-			exit(0);
-		}
-
-		pid_t pid = fork();
-		
-		if(pid < 0){
-			exit(EXIT_FAILURE);
-		} else 	if(pid == 0) {
-			execvp(argv[0], argv);
-			exit(EXIT_SUCCESS);
-		} else {
-			if(background == 0){
-				wait(&status);
+				if(pid < 0){
+					exit(EXIT_FAILURE);
+				} else 	if(pid == 0) {
+					execvp(argv[0], argv);
+					exit(EXIT_SUCCESS);
+				} else {
+					if(background == 0){
+						wait(&status);
+					}
+				}
+			}
+			
+			if(st == TKN_EOL){
+				break;
 			}
 		}
 	}
+}
+
+int check_state(enum TKN_KIND st, char* input)
+{
+	int rt = 0;
+	switch(st){
+		case TKN_BG:
+			// background
+			background = 1;
+			break;
+		case TKN_EOF:
+		case TKN_EOL:
+			rt = 1;
+			break;
+		default:
+			strncpy(lbuf, input, strlen(input));
+			getargs(&argc, argv, lbuf);
+			break;
+	}
+
+	return rt;
+}
+
+void run_command(int argc, char* argv[])
+{
+	if(strcmp(argv[0], "cd") == 0){
+		chdir(argv[1]);
+	//	continue;
+	} else if(strcmp(argv[0], "exit") == 0){
+		exit(0);
+	}
+
+	pid_t pid = fork();
+
+	if(pid < 0){
+		exit(EXIT_FAILURE);	
+	} else  if(pid == 0) {
+		execvp(argv[0], argv);
+		exit(EXIT_SUCCESS);
+	} else {
+		if(background == 0){
+	//		wait(&status);
+		}
+	}						
 }
